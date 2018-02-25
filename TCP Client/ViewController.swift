37ed87a226c86ef909e032ab82d2b4c132b34601
Pavan Kotesh <kpavankotesh@gmail.com>
2018-02-25 09:32:52 +0530

    Initial Commit

---
+++ TCP Client/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TCP Client
+//
+//  Created by Pavan Kotesh on 25/02/18.
+//  Copyright © 2018 Pavan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
